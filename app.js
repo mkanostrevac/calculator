33c501320d57eb35a5bf862b93853bac930de7a7
mkanostrevac <mkanostrevac@gmail.com>
2017-04-16 14:30:27 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+var ERROR = "syntax error";
+var ZERO = "0";
+
+var listTdTags = document.getElementsByTagName("td");
+
+function lineCalculation(expression) {
+
+  var result;
+
+  try {
+    result = eval(expression);
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      result = "syntax error";
+    }
+  }
+
+  return result;
+}
+
+for (var i = 0; i < listTdTags.length; i++) {
+
+  var tdTag = listTdTags[i];
+
+  if (!tdTag.id) {
+    tdTag.addEventListener("click", function(e) {
+      var el = event.target || event.srcElement;
+
+      var calculateLine = document.getElementById("calculate-line");
+
+      if (el.innerText === "AC") {
+        calculateLine.innerText = 0;
+      } else if (el.innerText === "=") {
+        calculateLine.innerText = lineCalculation(calculateLine.innerText);
+      } else {
+        if (calculateLine.innerText === ZERO || calculateLine.innerText === ERROR) {
+          calculateLine.innerText = el.innerText;
+        } else {
+          calculateLine.innerText += el.innerText;
+        }
+      }
+    });
+  }
+}
